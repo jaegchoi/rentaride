69b39f54d01772cc7e8b4a6c3ec7ebeba6bf0dcc
mat704 <32942209+mat704@users.noreply.github.com>
2017-10-22 18:25:01 -0400

    Update VehicleTypeImpl.java

--- rentaride/src/edu/uga/cs/rentaride/entity/impl/VehicleTypeImpl.java
+++ rentaride/src/edu/uga/cs/rentaride/entity/impl/VehicleTypeImpl.java
@@ -1,5 +1,76 @@
 package edu.uga.cs.rentaride.entity.impl;
 
-public class VehicleTypeImpl {
+import java.util.List;
 
+import edu.uga.cs.rentaride.Vehicle;
+import edu.uga.cs.rentaride.HourlyPrice;
+import edu.uga.cs.rentaride.Reservation;
+import edu.uga.cs.rentaride.RARException;
+import edu.uga.cs.rentaride.persistence.Persistable;
+
+
+
+public class VehicleTypeImpl
+    extends Persistent implements VehicleType
+{
+   public String name;
+   public List<HourlyPrice> priceList;
+   public List<Vehicle> vehicleList;
+   public List<Reservation> reservationList;
+
+public VheicleTypeImpl(){
 }
+
+public VehicleTypeImpl(String name, HourlyPrice price, Vehicle vehicle, Reservation reservation){
+    this.name = name;
+    
+    
+}
+
+@Override
+    public String getName(){
+      return name;
+    }
+    
+    @Override
+    public void setName( String name ) throws RARException{
+      this.name = name;
+    }
+    
+    @Override
+    public List<HourlyPrice> getHourlyPrices(){
+        if(priceList == null){
+          if(isPersistent()){
+            HourlyPrice p = new HourlyPriceImpl();
+            p.setVehickeType(this);
+            priceList = getPersistenceLayer().restoreHourlyPrice(p);
+          }
+        }
+        return priceList;
+    }
+   
+   @Override
+       public List<Vehicle> getVehicles(){
+       if(vehicleList == null){
+          if(isPersistent()){
+            Vehicle v = new VehicleImpl();
+            v.setVehickeType(this);
+            vehicleList = getPersistenceLayer().restoreVehicle(v);
+          }
+        }
+        return vehicleList;
+       }
+    
+   @Override
+    public List<Reservation> getReservations(){
+      if(reservationList == null){
+          if(isPersistent()){
+            Reservation r = new ReservationImpl();
+            r.setVehickeType(this);
+            reservationList = getPersistenceLayer().restoreReservation(r);
+          }
+        }
+        return reservationList;
+    }
+}
+
