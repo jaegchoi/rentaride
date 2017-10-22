b0a6a4fbe2e144d38dc08bd2b9fa8b1f4131f457
mat704 <32942209+mat704@users.noreply.github.com>
2017-10-22 17:45:46 -0400

    Update ReservationImpl.java

--- rentaride/src/edu/uga/cs/rentaride/entity/impl/ReservationImpl.java
+++ rentaride/src/edu/uga/cs/rentaride/entity/impl/ReservationImpl.java
@@ -1,5 +1,85 @@
 package edu.uga.cs.rentaride.entity.impl;
 
-public class ReservationImpl {
+import java.util.Date;
+
+import edu.uga.cs.rentaride.RARException;
+import edu.uga.cs.rentaride.persistence.impl.Persistable;
+
+
+public ReservationImpl extends Persistent implements Reservation {
+    public Date pickup;
+    public int length;
+    public boolean cancelled;
+    public RentalLocation location;
+    public VehicleType type;
+    public Customer customer;
+    public Rental rental;
+  
+  public ReservationImpl(){
+  
+  }
+  public ReservationImpl(Date pickup, int length, boolean cancelled, RentalLocation location, VehicleType type, Customer customer, Rental rental){
+    
+    this.pickup = pickup;
+    this.length = length;
+    this.cancelled = cancelled;
+    this.localtion = location;
+    this.type = type;
+    this.customer = customer;
+    this.rental = rental;
+  }
+  
+  @Override
+    public Date getPickupTime(){
+        return pickup;
+    }
+  @Override
+    public void setPickupTime( Date pickupTime ) throws RARException{
+          this.pickup = pickup;
+    }
+  @Override
+    public int getLength(){
+     return length; 
+    }
+    
+@Override
+    public void setLength( int length ) throws RARException{
+        this.length = length; 
+    }
+@Override    
+    public Customer getCustomer(){
+      return customer; 
+    }
+@Override    
+    public void setCustomer( Customer customer ) throws RARException{
+      this.customer = customer;
+    }
+@Override    
+    public VehicleType getVehicleType(){
+     return type; 
+    }
+    
+@Override
+    public void setVehicleType( VehicleType vehicleType ) throws RARException{
+      this.type = type;
+}
+  @Override
+    public RentalLocation getRentalLocation(){
+      return location;
+    
+    }  
+  @Override
+    public void setRentalLocation( RentalLocation rentalLocation ) throws RARException{
+      this.location = location; 
+    }
+    
+  @Override
+    public Rental getRental(){
+     return rental; 
+    }
+@override
+    public void setRental( Rental rental ){
+      this.rental = rental;  
+    }
 
 }
