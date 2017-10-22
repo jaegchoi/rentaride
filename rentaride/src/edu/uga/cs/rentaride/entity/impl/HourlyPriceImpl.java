6c86f5d2ebec297ddefd942de4c118b0b9b723ff
mat704 <32942209+mat704@users.noreply.github.com>
2017-10-22 18:45:48 -0400

    Update HourlyPriceImpl.java

--- rentaride/src/edu/uga/cs/rentaride/entity/impl/HourlyPriceImpl.java
+++ rentaride/src/edu/uga/cs/rentaride/entity/impl/HourlyPriceImpl.java
@@ -1,5 +1,55 @@
 package edu.uga.cs.rentaride.entity.impl;
 
-public class HourlyPriceImpl {
+import edu.uga.cs.rentaride.entity.HourlyPrice;
+import edu.uga.cs.rentaride.entity.VehicleType;
+import edu.uga.cs.rentaride.RARException;
+import edu.uga.cs.rentaride.persistence.impl.Persistent;
 
+
+public class HourlyPriceImpl
+    extends Persistent implements HourlyPrice
+{   
+    public int maxHours;
+    public int price;
+    public VehicleType type;
+    
+    public HourlyPriceImpl(){
+    
+   }
+   
+   public HourlyPriceImpl(int maxHours, int price, VehicleType vehicle){
+      this.maxHours = maxHours;
+      this.price = price;
+      this.vehicle = vehicle;
+   
+   }
+    
+   @Override
+    public int getMaxHours(){
+      return maxHours;
+    }
+    
+@Override
+public void setMaxHours( int maxHours ) throws RARException{
+    this.maxHours = maxHours;
+}
+ 
+ @Override
+    public int getPrice(){
+    return price;
+    }
+    @OVerride
+    public void setPrice( int price ) throws RARException{
+      this.price = price;
+    }
+    
+    @Override
+    public VehicleType getVehicleType(){
+      return vehicleType;
+    }
+
+@Override
+public void setVehicleType( VehicleType vehicleType ) throws RARException{
+  this.type = vehicleType;
+}
 }
