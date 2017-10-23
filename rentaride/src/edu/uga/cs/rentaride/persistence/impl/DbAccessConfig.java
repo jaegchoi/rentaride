d733356f42c14e58eda875311ffe4ba5dfde9381
jah25717 <jah25717@uga.edu>
2017-10-23 15:50:09 -0400

    Create DbAccessConfig.java

---
+++ rentaride/src/edu/uga/cs/rentaride/persistence/impl/DbAccessConfig.java
@@ -0,0 +1,10 @@
+package edu.uga.cs.rentaride.persistence.impl;
+
+public abstract class DbAccessConfig {
+	static final String DB_DRIVE_NAME="com.mysql.jdbc.Drive";
+	static final String DB_NAME="rentaride";
+	static final String DB_SERVER_NAME="localhost";
+	static final String DB_CONNECTION_URL="jdbc:mysql://localhost:3306/rentaride";
+	static String DB_CONNECTION_USERNAME="demo";
+	static String DB_CONNECTION_PWD="demo";
+}
