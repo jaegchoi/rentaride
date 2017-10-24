677a5ff992b8ed0157e18d9ef6d81306165747ce
jah25717 <jah25717@uga.edu>
2017-10-23 20:31:14 -0400

    finished save method
    still need to do delete and restore

--- rentaride/src/edu/uga/cs/rentaride/persistence/impl/VehicleManager.java
+++ rentaride/src/edu/uga/cs/rentaride/persistence/impl/VehicleManager.java
@@ -47,81 +47,111 @@ class VehicleManager{
 			stmt.setString(1, vehicle.getMake());
 		}
 		else{
-			throw new RARException( "VehicleManager.save: can't save a Vehicle: make undefined" );
+			stmt.setNull(1, java.sql.Types.VARCHAR);
 		}
 		
 		
 		if(vehicle.getModel()!=null){
-			
+			stmt.setString(2, vehicle.getModel());
 		}
 		else{
-			
+			stmt.setNull(2, java.sql.Types.VARCHAR);
 		}
 		
 		
 		if(vehicle.getYear()!=0){
-			
+			stmt.setInt(3, vehicle.getYear());
 		}
 		else{
-			
+			stmt.setNull(3, java.sql.Types.INTEGER);
 		}
 		
 		
 		if(vehicle.getMileage()!=0){
-			
+			stmt.setInt(4, vehicle.getMileage());
 		}
 		else{
-			
+			stmt.setNull(4, java.sql.Types.INTEGER);
 		}
 		
 		
 		if(vehicle.getRegistrationTag()!=null){
-			
+			stmt.setString(5, vehicle.getRegistrationTag());
 		}
 		else{
-			
+			throw new RARException("VehicleManager: can't save a vehicle: Tag undefined");
 		}
 		
 		
 		if(vehicle.getLastServiced()!=null){
-			
+			java.util.Date jDate = vehicle.getLastServiced();
+			java.sql.Date sDate = new java.sql.Date( jDate.getTime() );
+			stmt.setDate(6, sDate);
 		}
 		else{
-			
+			stmt.setNull(6, java.sql.Types.DATE);
 		}
 		
 		
 		if(vehicle.getStatus()!=null){
-			
+			stmt.setString(7, vehicle.getStatus().toString());
 		}
 		else{
-			
+			stmt.setNull(7, java.sql.Types.VARCHAR);
 		}
 		
 		
 		if(vehicle.getCondition()!=null){
-			
+			stmt.setString(8, vehicle.getCondition().toString());
 		}
 		else{
-			
+			stmt.setNull(8, java.sql.Types.VARCHAR);
 		}
 		
 		
 		if(vehicle.getRentalLocation()!=null){
-			
+			stmt.setString(9, vehicle.getRentalLocation().toString());
 		}
 		
 		else{
-			
+			stmt.setNull(9, java.sql.Types.VARCHAR);
 		}
 		
 		
 		if(vehicle.getVehicleType()!=null){
-			
+			stmt.setString(10, vehicle.getVehicleType().toString());
 		}
 		else{
-			
+			stmt.setNull(10, java.sql.Types.VARCHAR);
 		}
+		
+		inscnt=stmt.executeUpdate();
+		
+		if(!vehicle.isPersistent()){
+			//if it is being created for the first time we need to assign an id
+			if(inscnt==1){
+				String sql="select last_insert_id()";
+				if(stmt.execute(sql)){
+					//retrieve result
+					ResultSet r=stmt.getResultSet();
+					//use only the first row
+					while(r.next()){
+						vehicleId=r.getLong(1);
+						if(vehicleId>0){
+							vehicle.setId(vehicleId);
+						}//if
+					}//while
+				}//if
+			}//if
+		
+			else{
+				if(inscnt<1){
+					throw new RARException("VehicleManager.save: failed to save a vehicle");
+				}//if
+			}//else
+			
+		}//if
+		
 		
 		}//try
 		catch(SQLException e){
