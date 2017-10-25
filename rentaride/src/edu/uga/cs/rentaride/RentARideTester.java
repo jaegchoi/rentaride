7867e304daf9917939cd95439b9c3b0ebf242371
jah25717 <jah25717@uga.edu>
2017-10-25 14:18:21 -0400

    finished tester file
    changes are according to Kochut's requirements doc

--- rentaride/src/edu/uga/cs/rentaride/RentARideTester.java
+++ rentaride/src/edu/uga/cs/rentaride/RentARideTester.java
@@ -1,4 +1,3 @@
-
 package edu.uga.cs.rentaride.test;
 
 import java.sql.Connection;
@@ -29,17 +28,26 @@ public class RentARideTester
          ObjectLayer objectLayer = null;
          PersistenceLayer persistence = null;
 		 
-		 Administrator admin;
-		 RentalLocation location;
-		 VehicleType type;
-		 HourlyPrice price;
-		 Vehicle v;
-		 Customer c;
-		 Reservation r;
-		 Rental rental;
-		 Comment comment;
-		 VehicleCondition vc = null;
-		 VehicleStatus vs=null;
+		 Administrator admin,admin2;
+		 
+		 RentalLocation location,locat2;
+		 
+		 VehicleType type,type2;
+		 
+		 HourlyPrice price,price2,price3,price4;
+		 
+		 Vehicle v,v2,v3,v4;
+		 
+		 Customer c,c2;
+		 
+		 Reservation r,r2,r3,r4;
+		 
+		 Rental rental,rental2;
+		 
+		 Comment comment,comment2;
+		 
+		 final VehicleCondition vc = VehicleCondition.GOOD;
+		 final VehicleStatus vs = VehicleStatus.INLOCATION;
 		 
 		 // get a database connection
          try {
@@ -59,74 +67,145 @@ public class RentARideTester
       
          persistence = new PersistenceLayerImpl( conn, objectLayer ); 
  
+         objectLayer.setPersistence( persistence );
 		 
 		 try{
 			 
-			 //admins
+			 //initialize Admins
 			 admin = objectLayer.createAdministrator("Jake", "Hilmandolar", "jhilm", "pass", "jhilm@gmail.com", "123 Elm Street", new Date());
 			 persistence.storeAdministrator(admin);
+			 admin2 = objectLayer.createAdministrator("Bilbo", "Baggins", "1ring", "pass", "torule@gmail.com", "Them All Court", new Date());
+			 persistence.storeAdministrator(admin2);
 			 
-			 //locations
+			 //init locations
 			 location = objectLayer.createRentalLocation("Target", "45 Atlanta Highway", 25);
 			 persistence.storeRentalLocation(location);
+			 locat2 = objectLayer.createRentalLocation("Zaxby's", "1900 Epps Bridge", 22);
+			 persistence.storeRentalLocation(locat2);
 			 
-			 //vehicle types with hourly prices
+			 // initialize vehicle types with hourly prices
 			 type = objectLayer.createVehicleType("truck");
 			 persistence.storeVehicleType(type);
-			 price = objectLayer.createHourlyPrice(2,10, type);
+			 type2=objectLayer.createVehicleType("car");
+			 persistence.storeVehicleType(type2);
+			 
+			 price = objectLayer.createHourlyPrice(3,15, type);
+			 price2=objectLayer.createHourlyPrice(6,17,type);
+			 price3=objectLayer.createHourlyPrice(1,8,type2);
+			 price4=objectLayer.createHourlyPrice(2,9,type2);
+			 
 			 persistence.storeHourlyPrice(price);
-			 persistence.storeVehicleType(type);
+			 persistence.storeHourlyPrice(price2);
+			 persistence.storeHourlyPrice(price3);
+			 persistence.storeHourlyPrice(price4);
 			 
 			 
-			 //vehicles
-			 v = objectLayer.createVehicle("Toyota", "Tacoma", 2013, "1234567", 10000, new Date(), type, location, vc, vs);
+			 
+			 
+			 //initialize vehicles
+			 v = objectLayer.createVehicle("Toyota", "Tacoma", 2006, "1234567", 90000, new Date(), type, location, vc, vs);
 			 persistence.storeVehicle(v);
-			 //customers
 			 
-			 c = objectLayer.createCustomer("Barney", "Stinson", "legen", "dairy", "highfive@gmail.com", "NY,NY", new Date(),
-			 new Date(), "New York", "12300", "111-111-111-111", new Date());
+			 v2 = objectLayer.createVehicle("Toyota", "Tundra", 2008, "1234567", 85000, new Date(), type, location, vc, vs);
+			 persistence.storeVehicle(v2);
+			 
+			 v3 = objectLayer.createVehicle("Toyota", "Corolla", 2015, "1234567", 30000, new Date(), type2, location, vc, vs);
+			 persistence.storeVehicle(v3);
+			 
+			 v4 = objectLayer.createVehicle("Honda", "Civic", 2017, "1234567", 3500, new Date(), type2, location, vc, vs);
+			 persistence.storeVehicle(v4);
+			 
+			 
+			 
+			 //initialize customer
+			 c = objectLayer.createCustomer("Barney", "Stinson", "legen", "dary", "highfive@gmail.com", "NY,NY", new Date(),
+			 new Date(), "NY", "12300", "111-111-111-111", new Date());
 			 persistence.storeCustomer(c);
-			 //reservations
 			 
-			 r = objectLayer.createReservation(new Date(), 2, type, location, c);
-			 persistence.storeReservation(r);
-			 //rentals
+			 c2 = objectLayer.createCustomer("Todd", "Gurley", "GoDawgs1", "LARAMS", "tgIII@gmail.com", "Athens,GA", new Date(),
+					 new Date(), "GA", "12356", "222-222-222-222", new Date());
+					 persistence.storeCustomer(c2);
 			 
+			//initialize reservation
+			r = objectLayer.createReservation(new Date(), 2, type, location, c);
+			r2 = objectLayer.createReservation(new Date(), 1, type2, locat2, c);
+			r3 = objectLayer.createReservation(new Date(), 1, type2, location, c2);
+			r4 = objectLayer.createReservation(new Date(), 2, type, locat2, c2);
+			persistence.storeReservation(r);
+			persistence.storeReservation(r2);
+			persistence.storeReservation(r3);
+			persistence.storeReservation(r4);
+			 
+			 //initialize rentals
 			 rental = objectLayer.createRental(new Date(), r, v);
+			 rental2=objectLayer.createRental(new Date(),r2,v4);
 			 persistence.storeRental(rental);
+			 persistence.storeRental(rental2);
 
-			 //comments
+			 //initialize comments
 			 comment = objectLayer.createComment("AWESOME RIDE MANNNNNN", new Date(), rental);
+			 comment2 = objectLayer.createComment("Car smells like smoke.", new Date(), rental2);
 			 persistence.storeComment(comment);
-
+			 persistence.storeComment(comment2);
 			 
-			 //delete the data
+			 //initialize delete the data
 			 persistence.deleteComment(comment);
 			 objectLayer.deleteComment(comment);
-
+			 persistence.deleteComment(comment2);
+			 objectLayer.deleteComment(comment2);
+			 
+			 
 			 persistence.deleteRental(rental);
 			 objectLayer.deleteRental(rental);
+			 persistence.deleteRental(rental2);
+			 objectLayer.deleteRental(rental2);
 
 			 persistence.deleteReservation(r);
 			 objectLayer.deleteReservation(r);
+			 persistence.deleteReservation(r2);
+			 objectLayer.deleteReservation(r2);
+			 persistence.deleteReservation(r3);
+			 objectLayer.deleteReservation(r3);
+			 persistence.deleteReservation(r4);
+			 objectLayer.deleteReservation(r4);
 
 			 persistence.deleteCustomer(c);
 			 objectLayer.deleteCustomer(c);
+			 persistence.deleteCustomer(c2);
+			 objectLayer.deleteCustomer(c2);
 
 			 persistence.deleteVehicle(v);
 			 objectLayer.deleteVehicle(v);
+			 persistence.deleteVehicle(v2);
+			 objectLayer.deleteVehicle(v2);
+			 persistence.deleteVehicle(v3);
+			 objectLayer.deleteVehicle(v3);
+			 persistence.deleteVehicle(v4);
+			 objectLayer.deleteVehicle(v4);
 
 			 persistence.deleteHourlyPrice(price);
 			 objectLayer.deleteHourlyPrice(price);
+			 persistence.deleteHourlyPrice(price2);
+			 objectLayer.deleteHourlyPrice(price2);
+			 persistence.deleteHourlyPrice(price3);
+			 objectLayer.deleteHourlyPrice(price3);
+			 persistence.deleteHourlyPrice(price4);
+			 objectLayer.deleteHourlyPrice(price4);
 
 			 persistence.deleteVehicleType(type);
 			 objectLayer.deleteVehicleType(type);
+			 persistence.deleteVehicleType(type2);
+			 objectLayer.deleteVehicleType(type2);
 
 			 persistence.deleteRentalLocation(location);
 			 objectLayer.deleteRentalLocation(location);
+			 persistence.deleteRentalLocation(locat2);
+			 objectLayer.deleteRentalLocation(locat2);
 
 			 persistence.deleteAdministrator(admin);
 			 objectLayer.deleteAdministrator(admin);
+			 persistence.deleteAdministrator(admin2);
+			 objectLayer.deleteAdministrator(admin2);
 
 			 
 		 }
