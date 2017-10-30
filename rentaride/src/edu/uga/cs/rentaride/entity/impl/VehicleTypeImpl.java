6176be192e3abc948bf407c2a246cf42c5962444
jah25717 <jah25717@uga.edu>
2017-10-30 13:51:50 -0400

    Fixed Syntax Errors

--- rentaride/src/edu/uga/cs/rentaride/entity/impl/VehicleTypeImpl.java
+++ rentaride/src/edu/uga/cs/rentaride/entity/impl/VehicleTypeImpl.java
@@ -39,11 +39,11 @@ public VehicleTypeImpl(String name, HourlyPrice price, Vehicle vehicle, Reservat
     }
     
     @Override
-    public List<HourlyPrice> getHourlyPrices(){
+    public List<HourlyPrice> getHourlyPrices() throws RARException{
         if(priceList == null){
           if(isPersistent()){
             HourlyPrice p = new HourlyPriceImpl();
-            p.setVehickeType(this);
+            p.setVehicleType(this);
             priceList = getPersistenceLayer().restoreHourlyPrice(p);
           }
         }
@@ -51,11 +51,11 @@ public VehicleTypeImpl(String name, HourlyPrice price, Vehicle vehicle, Reservat
     }
    
    @Override
-       public List<Vehicle> getVehicles(){
+       public List<Vehicle> getVehicles() throws RARException{
        if(vehicleList == null){
           if(isPersistent()){
             Vehicle v = new VehicleImpl();
-            v.setVehickeType(this);
+            v.setVehicleType(this);
             vehicleList = getPersistenceLayer().restoreVehicle(v);
           }
         }
@@ -63,15 +63,21 @@ public VehicleTypeImpl(String name, HourlyPrice price, Vehicle vehicle, Reservat
        }
     
    @Override
-    public List<Reservation> getReservations(){
+    public List<Reservation> getReservations() throws RARException{
       if(reservationList == null){
           if(isPersistent()){
             Reservation r = new ReservationImpl();
-            r.setVehickeType(this);
+            r.setVehicleType(this);
             reservationList = getPersistenceLayer().restoreReservation(r);
           }
         }
         return reservationList;
     }
+
+@Override
+public String getType() {
+	// TODO Auto-generated method stub
+	return null;
+}
 }
 
